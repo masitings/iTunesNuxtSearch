2f5ab7a63f234131f6d9c3c1d3225a7bc3fa1c30
Rafi Bagaskara Halilintar <masitingss@gmail.com>
2019-02-24 06:21:11 +0700

    First

---
+++ components/card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="e3" style="max-width: 400px; margin:auto;" class="grey lighten-3">
+    <v-container fluid style="min-height: 0;" grid-list-lg>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card target="_blank" :href="url" :color="color" class="white--text">
+            <v-layout row>
+              <v-flex xs7>
+                <v-card-title primary-title>
+                  <div>
+                    <div class="headline">{{ title }}</div>
+                    <div>{{ artistName }}</div>
+                    <div>(2013)</div>
+                  </div>
+                </v-card-title>
+              </v-flex>
+              <v-flex xs5>
+                <v-img :src="image" height="100px" contain></v-img>
+              </v-flex>
+            </v-layout>
+            <v-divider light></v-divider>
+            <v-card-actions class="pa-3">Rate this album
+              <v-spacer></v-spacer>
+              <v-icon>star_border</v-icon>
+              <v-icon>star_border</v-icon>
+              <v-icon>star_border</v-icon>
+              <v-icon>star_border</v-icon>
+              <v-icon>star_border</v-icon>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+  props: ["title", "image", "artistName", "url", "color"]
+};
+</script>
